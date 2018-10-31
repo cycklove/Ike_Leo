8d57231babf49e5f9f01b272d87f764111bcbdd4
Ike_Leo <saiyuyu3@163.com>
2018-10-31 17:18:54 +0800

    By ChenBan

---
+++ OOP/01.py
@@ -0,0 +1,26 @@
+'''
+定义一个学生类 用来形容学生
+'''
+#定义一个空的类
+class Student():
+    pass  #代表直接跳过
+
+# 定义一个对象
+mingyue = Student()
+
+class PythonStudent():
+    name = None
+    age = 18
+    course = "Python"
+
+    def dohomework(self):
+        print("I 在做作业")
+
+        return  None
+
+#实例化
+yueyue = PythonStudent()
+yueyue.name
+yueyue.age
+
+yueyue.dohomework()
