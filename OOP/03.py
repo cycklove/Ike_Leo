d0202e423e8d2d08e739595431e2866e4bb3ed70
Ike_Leo <saiyuyu3@163.com>
2018-11-28 17:19:12 +0800

    By ChenBan

--- OOP/03.py
+++ OOP/03.py
@@ -34,3 +34,40 @@ class Student(Person):
 
 stu = Student("leo")
 stu.work()
+
+# 菱形继承问题
+class A():
+    pass
+class B(A):
+    pass
+class C(A):
+    pass
+class D(B,C):
+    pass
+
+class Person2():
+    # 对Person2类进行实例化的时候
+    # 姓名要确定
+    # 年龄要确定
+    # 地址肯定有
+    def __init__(self):
+        self.name = "noname"
+        self.age = 18
+        self.address = "house"
+        print("in init func")
+
+p = Person2()
+
+# 如果子类没写构造函数 则自动向上查找 直到找到位置
+class A():
+    pass
+class B(A):
+    def __init__(self,name):
+        print("B")
+class C(B):
+    def __init__(self,name):
+        B.__init__(self,name)
+        print("这是C中附加的功能")
+
+#首先查找C的构造函数 如果没有 则向上按照MRO顺序查找父类的构造函数 直到找到为止
+c = C("我是C")
