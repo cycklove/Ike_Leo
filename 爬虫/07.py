a7b16978a6aec9f7ed4eb05b56c01cfdf834af0e
Ike_Leo <saiyuyu3@163.com>
2019-07-30 09:24:32 +0800

    By ChenBan

--- 爬虫/07.py
+++ 爬虫/07.py
@@ -13,7 +13,45 @@ def reg():
     data = {
         'kw': word
     }
-    print(word)
+    #print(word)
+    # 需要使用parse模块对data进行编码
+    data = parse.urlencode(data).encode("utf-8")
+
+    # 需要构造一个请求头 应该至少包含传入的数据的长度
+    # request要求传入的请求头是一个dict格式
+
+    headers = {
+        # 因为使用post 至少包含content-length 字段
+        "Content-Length": len(data)
+    }
+
+    rsp = request.urlopen(baseurl, data=data)
+
+    json_data = rsp.read().decode("utf-8")
+
+    json_data = json.loads(json_data)
+
+    test2 = {}
+
+    for item in json_data["data"]:
+        test2[item["k"]] = item["v"]
+
+    # ensure_ascii=False来指定出中文
+    # indent对json进行数据格式化输出
+    aa = json.dumps(test2,ensure_ascii=False, indent=4)
+
+    lb3["fg"] = "red"
+    lb3["text"] = aa
+
+def reg2(event):
+    baseurl = "https://fanyi.baidu.com/sug"
+    # 存放用来模拟from的数据 一定是dict格式
+    word = e1.get()
+
+    data = {
+        'kw': word
+    }
+    #print(word)
     # 需要使用parse模块对data进行编码
     data = parse.urlencode(data).encode("utf-8")
 
@@ -57,7 +95,7 @@ e1.pack()
 
 btn = tkinter.Button(baseframe,text="确定",command = reg,fg="black")
 #btn.grid(row=2,column=1,stick=tkinter.E)
-btn.bind("<Return>",reg)
+btn.bind("<Return>",reg2)
 btn.pack()
 
 lb3 = tkinter.Label(baseframe, text="")
