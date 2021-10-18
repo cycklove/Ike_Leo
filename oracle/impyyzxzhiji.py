4a352f73717f68c3067943c08e044051bba1be64
cycklove <saiyuyu3@163.com>
2021-10-18 16:46:30 +0800

    By ChenBan

--- oracle/impyyzxzhiji.py
+++ oracle/impyyzxzhiji.py
@@ -6,17 +6,17 @@ from openpyxl.styles import Font, colors, Alignment
 import cx_Oracle as oracle
 
 def qingkongtable():
-    db = oracle.connect('ddqnc63/ddqnc63@192.168.50.31:1521/ncprd')
-    cursor = db.cursor()
-    sql = 'truncate table FR_OAYYZXZHIJI'
-    cursor.execute(sql)
-    db.commit()
-    cursor.close()
-    db.close()
+    db = oracle.connect('ddqnc63/ddqnc63@192.168.50.31:1521/ncprd') #配置数据库连接
+    cursor = db.cursor() #打开接收返回值方法
+    sql = 'truncate table FR_OAYYZXZHIJI2' #需要执行的语句
+    cursor.execute(sql)  #执行语句
+    db.commit() #提交结果
+    cursor.close() #关闭接收返回值方法
+    db.close() #关闭数据库连接
 
 
 def selectPath():
-    path_ = askopenfilename()
+    path_ = askopenfilename() #调用文件选择框
     path.set(path_)
 
 
@@ -47,18 +47,20 @@ def insert_deta():
                 row_data.append(ws.cell(row=rx, column=cx).value)
             i = i+1
             jdt['value'] = i + 1
-            sql = "INSERT INTO FR_OAYYZXZHIJI(XM,YIJIBUMEN,ERJIBUMEN,SANJIBUMEN,QY,YF,ZHIWEI,ZHIJI,TIAOZHENGHOUZHIJI,GANGWEI,NF,SYNCTIME,PP,奖励标准小榨,奖励标准双低压榨菜籽油,占比系数,奖励标准,市场类别,five奖励标准)VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,sysdate,:13,:14,:15,:16,:17,:18,:19)"
+            sql = "INSERT INTO FR_OAYYZXZHIJI2(XM,YIJIBUMEN,ERJIBUMEN,SANJIBUMEN,QY,YF,ZHIWEI,ZHIJI,TIAOZHENGHOUZHIJI,GANGWEI,NF,SYNCTIME,PP,奖励标准小榨,奖励标准双低压榨菜籽油,占比系数,奖励标准,市场类别,five奖励标准)VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,sysdate,:13,:14,:15,:16,:17,:18,:19)"
             data = (row_data[0], row_data[1],row_data[2], row_data[3],row_data[4], row_data[5],row_data[6], row_data[7],row_data[8], row_data[9],row_data[10],row_data[11],row_data[12],row_data[13],row_data[14],row_data[15],row_data[16],row_data[17])
 
             print(data)
             cursor.execute(sql,data) # 执行sql语句
             row_data = []
             baseframe.update()
+            lb6["text"] = i
             print(i)
             db.commit()
         cursor.close()
         db.close()# 关闭连接
 
+        lb6["text"] = ""
         lb3["fg"] = "GREEN"
         lb3["text"] = '导入成功,共计%s条!'%i
     except Exception as e:
@@ -71,7 +73,7 @@ baseframe = Tk()
 baseframe.wm_title("道道全营销中心职级导入")
 baseframe.geometry("500x400")
 
-path = StringVar()
+path = StringVar() #字符串变量”对象，可以与Entry、Label等控件绑定，这里的绑定是双向绑定，也就是既可以通过该变量来获取Entry、Label等控件中的值，也可以通过更改该变量来改变Entry、Label等控件中的值
 
 photo = PhotoImage(file="bg.gif")#file：t图片路径
 
@@ -111,5 +113,8 @@ lb5.place(x=60,y=100)
 jdt = tkinter.ttk.Progressbar(baseframe,length=250)
 jdt.place(x=135,y=100)
 
+lb6 = Label(baseframe,text="",bg="white",fg="green",font=("微软雅黑",15))
+# lb1.pack()
+lb6.place(x=385,y=95)
 
 baseframe.mainloop()
